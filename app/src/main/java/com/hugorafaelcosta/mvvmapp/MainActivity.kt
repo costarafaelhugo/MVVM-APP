d100e1268d0d4ccf61c1d94ee97edc36deabaa17
Hugo Costa <costarafaelhugo@gmail.com>
2022-02-23 11:46:48 -0300

    Implementei o layout da recycler view

--- app/src/main/java/com/hugorafaelcosta/mvvmapp/MainActivity.kt
+++ app/src/main/java/com/hugorafaelcosta/mvvmapp/MainActivity.kt
@@ -1,9 +1,23 @@
 package com.hugorafaelcosta.mvvmapp
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import androidx.lifecycle.ViewModel
+import com.hugorafaelcosta.mvvmapp.adapter.MainAdapter
+import com.hugorafaelcosta.mvvmapp.databinding.ActivityMainBinding
+import com.hugorafaelcosta.mvvmapp.rest.RetrofitService
 
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var bindig : ActivityMainBinding
+
+    lateinit var viewModel: ViewModel
+
+    private val retrofitService = RetrofitService.getInstance()
+
+    private val adapter = MainAdapter{
+
+    }
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
